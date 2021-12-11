c65b5c4f123e8ff530e89b5d8cbf02707dc35bb9
renzodupont <75647168+renzodupont@users.noreply.github.com>
2021-12-11 19:05:35 -0300

    Jest Test

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", async (req, res) => {
+  res.json({ message: "Hello World!" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+module.exports = app;
---
+++ index.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("./index");
+
+it("Hello World", async () => {
+  const agent = request.agent(app);
+  const res = await agent.get("/");
+  expect(res.body.message).toBe("Hello World!");
+});
